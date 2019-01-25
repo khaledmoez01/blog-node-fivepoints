ce5e51487c68b5e628ba2a661223194388ffec45
khaledmoez <khaledmoez@yahoo.fr>
2019-01-25 07:44:51 +0100

    nettoyage de code dans appJS

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use(function(err, req, res, next) {
 
 let port = process.env.PORT || 3000;
 
-let server = app.listen(port, function() {
+app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
