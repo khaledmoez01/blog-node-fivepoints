0c6bc455b9a81391c84c60dfca561763b630e324
khaledmoez <khaledmoez@yahoo.fr>
2019-01-25 12:40:15 +0100

    firstCommit branch array

---
+++ README.md
@@ -0,0 +1,4 @@
+pour lancer l'application en local:
+    npm run start_local
+
+il faut initialiser les variables d'environnements dans un fichier ".env" (dans le répertoire "root" du projet) à créer en suivant l'exemple du fichier .env.example
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let router  = express.Router();
+let auth    = require('./auth');
+
+// Require controller modules.
+let index_controller = require('../controllers/indexController');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.send('Hello World!');
+});
+
+//01   body(firstName, lastName, email, password, role) - Création d’un user
+router.post('/signup', auth.optional, index_controller.index_signup_post);  // kmg done
+
+//02   body(email, password) - Authentification d’un user
+router.post('/login', auth.optional, index_controller.index_login_post);  // kmg done
+
+module.exports = router;
